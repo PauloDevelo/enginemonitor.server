1bf1b6354e112804c70d7917cd2989848c7b7c13
Torruella, Paul <paul.torruella@suez.com>
2019-04-24 08:04:33 +0800

    rename the batch file to update the mongodb

--- updateEquipmentMaintenanceDB.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\MongoDB\Server\4.0\bin\mongo.exe" --eval "const databaseName = 'enginemaintenancedev'" ./database/createAndUpdateMongoDB.js
---
+++ updateEquipmentMaintenanceDB.cmd
@@ -0,0 +1,1 @@
+mongo --eval "const databaseName = 'enginemaintenancedev'" ./database/createAndUpdateMongoDB.js
