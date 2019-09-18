24acf18c71f64676eccfbfde6ec44ebe4dbeda35
Torruella, Paul <paul.torruella@suez.com>
2019-09-18 08:37:31 +0800

    improve the wrapAsync

--- src/utils/expressHelpers.ts
+++ src/utils/expressHelpers.ts
@@ -1,7 +1,11 @@
-export default function wrapAsync(fn) {
-    return function(req, res, next) {
+ const wrapAsync = (fn) => {
+    const asyncFuncWrapperHandler = (req, res, next) => {
       // Make sure to `.catch()` any errors and pass them along to the `next()`
       // middleware in the chain, in this case the error handler.
       fn(req, res, next).catch(next);
     };
-  }
+
+    return asyncFuncWrapperHandler;
+  };
+
+ export default wrapAsync;
