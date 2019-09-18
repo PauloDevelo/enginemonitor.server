f3666bd78e787c296c52f0ed392d90ad1b9b1de9
Torruella, Paul <paul.torruella@suez.com>
2019-09-18 08:36:38 +0800

    Add a storage limit check when uploading a new image

--- src/utils/uploadMulter.ts
+++ src/utils/uploadMulter.ts
@@ -1,8 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import fs from "fs";
+import getSize from "get-folder-size";
 import multer from "multer";
+
+import config from "./configUtils";
+
+import {getUser} from "./requestContext";
+
+const getImageDirectory = (): string => {
+    const user = getUser();
+    return "./uploads/" + user._id;
+};
 
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        cb(null, "./uploads/");
+        const dir = getImageDirectory();
+
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+
+        cb(null, dir);
     },
     filename(req, file, cb) {
         cb(null, Date.now() + file.originalname);
@@ -24,5 +42,30 @@ const upload = multer({
     },
     storage,
 });
+
+const getFolderSize = (myFolder: string): Promise<number> => {
+    return new Promise((resolve, reject) => {
+        getSize(myFolder, (err: any, size: number) => {
+            if (err) {
+                reject(err);
+            }
+
+            resolve(size);
+        });
+    });
+};
+
+export const checkImageQuota = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const dir = getImageDirectory();
+    const folderSize = await getFolderSize(dir);
+
+    const folderSizeLimit = config.get("userImageFolderLimitInByte");
+
+    if (folderSize > folderSizeLimit) {
+        throw new Error("userExceedStorageLimit");
+    }
+
+    next();
+};
 
 export default upload;
