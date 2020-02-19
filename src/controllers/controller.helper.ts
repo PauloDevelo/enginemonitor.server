c6af79ae63ef60e51a46a59785f57252c6647526
Torruella, Paul <paul.torruella@suez.com>
2020-02-19 21:27:13 +0200

    Add a new checkUserCredential function

--- src/controllers/controller.helper.ts
+++ src/controllers/controller.helper.ts
@@ -5,8 +5,8 @@ import AssetUser from "../models/AssetUser";
 
 import {getUser} from "../utils/requestContext";
 
-export const checkCredentials = async (req: express.Request, res: express.Response, next: any) => {
-  switch(req.method){
+export const checkUserCredentials = async (method: string, assetUiId: string, res: express.Response, next: any) => {
+  switch(method){
       case 'GET':
           next();
           break;
@@ -14,7 +14,7 @@ export const checkCredentials = async (req: express.Request, res: express.Respon
       case 'DELETE':
       default:
           const user = getUser();
-          const asset = await getAssetByUiId(req.params.assetUiId);
+          const asset = await getAssetByUiId(assetUiId);
 
           try{
             const assetUser = await AssetUser.findOne({ assetId: asset._id, userId: user._id });
@@ -30,4 +30,8 @@ export const checkCredentials = async (req: express.Request, res: express.Respon
           
           return;
   }
+}
+
+export const checkCredentials = async (req: express.Request, res: express.Response, next: any) => {
+  checkUserCredentials(req.method, req.params.assetUiId, res, next);
 }
