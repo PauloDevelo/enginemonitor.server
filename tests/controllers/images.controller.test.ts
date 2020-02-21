fc06a92fa26341c2592953bb2a2b993dd075218e
Torruella, Paul <paul.torruella@suez.com>
2020-02-21 21:07:16 +0200

    Test the read only credential for the image controller.

--- tests/controllers/images.controller.test.ts
+++ tests/controllers/images.controller.test.ts
@@ -29,6 +29,10 @@ import AssetUser from '../../src/models/AssetUser';
 
 describe('Images', () => {
     let user: IUser;
+
+    let roUser: IUser;
+    let roUserJWT: string;
+
     let boat: IAssets;
     let engine: IEquipments;
 
@@ -49,11 +53,19 @@ describe('Images', () => {
         user.setPassword("test");
         user = await user.save();
 
+        roUser = new Users({ name: "read", firstname: "only", email: "read.only@gmail.com", forbidUploadingImage: true });
+        roUser.setPassword("test");
+        roUser = await roUser.save();
+        roUserJWT = `Token ${roUser.generateJWT()}`;
+
         boat = new Assets({_uiId: 'sailboat_01', brand: 'aluminium & techniques', manufactureDate: '1979/01/01', modelName: 'heliotrope', name: 'Arbutus',});
         boat = await boat.save();
 
         let assetUserLink = new AssetUser({ assetId: boat._id, userId: user._id });
         assetUserLink = await assetUserLink.save();
+
+        let assetRoUserLink = new AssetUser({ assetId: boat._id, userId: roUser._id, readonly: true });
+        assetRoUserLink = await assetRoUserLink.save();
 
         engine = new Equipments({name: "Engine", brand:"Nanni", model:"N3.30", age:1234, installation:"2018/01/20", _uiId:"engine_01"});
         engine.assetId = boat._id;
@@ -103,6 +115,106 @@ describe('Images', () => {
 
             // Act
             let res = await chai.request(app).get('/api/images/' + engine._uiId.toString()).set("Authorization", "Token " + user.generateJWT());
+
+            // Assert
+            res.should.have.status(200);
+            res.body.should.have.property("images");
+            res.body.images.should.be.a("array");
+            res.body.images.length.should.be.eql(3);
+
+            res.body.images[0].should.have.property("name");
+            res.body.images[0].name.should.be.eql("image1");
+
+            res.body.images[0].should.have.property("description");
+            res.body.images[0].description.should.be.eql("image 1 description");
+
+            res.body.images[0].should.have.property("parentUiId");
+            res.body.images[0].parentUiId.should.be.eql("engine_01");
+
+            res.body.images[0].should.have.property("sizeInByte");
+            res.body.images[0].sizeInByte.should.be.eql(297235);
+
+            res.body.images[0].should.have.property("thumbnailUrl");
+            res.body.images[0].thumbnailUrl.should.be.eql("http://localhost:8000/api/uploads/thumbnail1.jpeg");
+
+            res.body.images[0].should.have.property("title");
+            res.body.images[0].title.should.be.eql("image1");
+
+            res.body.images[0].should.have.property("url");
+            res.body.images[0].url.should.be.eql("http://localhost:8000/api/uploads/image1.jpeg");
+
+ 
+            res.body.images[1].should.have.property("name");
+            res.body.images[1].name.should.be.eql("image2");
+
+            res.body.images[1].should.have.property("description");
+            res.body.images[1].description.should.be.eql("image 2 description");
+
+            res.body.images[1].should.have.property("parentUiId");
+            res.body.images[1].parentUiId.should.be.eql("engine_01");
+
+            res.body.images[1].should.have.property("sizeInByte");
+            res.body.images[1].sizeInByte.should.be.eql(297235);
+
+            res.body.images[1].should.have.property("thumbnailUrl");
+            res.body.images[1].thumbnailUrl.should.be.eql("http://localhost:8000/api/uploads/thumbnail2.jpeg");
+
+            res.body.images[1].should.have.property("title");
+            res.body.images[1].title.should.be.eql("image2");
+
+            res.body.images[1].should.have.property("url");
+            res.body.images[1].url.should.be.eql("http://localhost:8000/api/uploads/image2.jpeg");
+
+
+            res.body.images[2].should.have.property("name");
+            res.body.images[2].name.should.be.eql("image3");
+
+            res.body.images[2].should.have.property("description");
+            res.body.images[2].description.should.be.eql("image 3 description");
+
+            res.body.images[2].should.have.property("parentUiId");
+            res.body.images[2].parentUiId.should.be.eql("engine_01");
+
+            res.body.images[2].should.have.property("sizeInByte");
+            res.body.images[2].sizeInByte.should.be.eql(297235);
+
+            res.body.images[2].should.have.property("thumbnailUrl");
+            res.body.images[2].thumbnailUrl.should.be.eql("http://localhost:8000/api/uploads/thumbnail3.jpeg");
+
+            res.body.images[2].should.have.property("title");
+            res.body.images[2].title.should.be.eql("image3");
+
+            res.body.images[2].should.have.property("url");
+            res.body.images[2].url.should.be.eql("http://localhost:8000/api/uploads/image3.jpeg");
+            
+        });
+
+        it('it should GET a 200 http code as a result for the read only user', async () => {
+            // Arrange
+            fs.copyFileSync('tests/toUpload/image1.jpeg', config.get("ImageFolder") + 'image1.jpeg');
+            fs.copyFileSync('tests/toUpload/thumbnail1.jpeg', config.get("ImageFolder") + 'thumbnail1.jpeg');
+            fs.copyFileSync('tests/toUpload/image2.jpeg', config.get("ImageFolder") + 'image2.jpeg');
+            fs.copyFileSync('tests/toUpload/thumbnail2.jpeg', config.get("ImageFolder") + 'thumbnail2.jpeg');
+            fs.copyFileSync('tests/toUpload/image3.jpeg', config.get("ImageFolder") + 'image3.jpeg');
+            fs.copyFileSync('tests/toUpload/thumbnail3.jpeg', config.get("ImageFolder") + 'thumbnail3.jpeg');
+
+            const image1Path = config.get("ImageFolder") + "image1.jpeg";
+            const thumbnail1Path = config.get("ImageFolder") + "thumbnail1.jpeg";
+            let image1 = new Images({ _uiId: "image_01", description: "image 1 description", name: "image1", parentUiId: "engine_01" , path:image1Path, thumbnailPath:thumbnail1Path, title:"image1"});
+            image1 = await image1.save();
+
+            const image2Path = config.get("ImageFolder") + "image2.jpeg";
+            const thumbnail2Path = config.get("ImageFolder") + "thumbnail2.jpeg";
+            let image2 = new Images({ _uiId: "image_02", description: "image 2 description", name: "image2", parentUiId: "engine_01" , path:image2Path, thumbnailPath:thumbnail2Path, title:"image2"});
+            image2 = await image2.save();
+
+            const image3Path = config.get("ImageFolder") + "image3.jpeg";
+            const thumbnail3Path = config.get("ImageFolder") + "thumbnail3.jpeg";
+            let image3 = new Images({ _uiId: "image_03", description: "image 3 description", name: "image3", parentUiId: "engine_01" , path:image3Path, thumbnailPath:thumbnail3Path, title:"image3"});
+            image3 = await image3.save();
+
+            // Act
+            let res = await chai.request(app).get('/api/images/' + engine._uiId.toString()).set("Authorization", roUserJWT);
 
             // Assert
             res.should.have.status(200);
@@ -288,6 +400,23 @@ describe('Images', () => {
             expect(config.get("ImageFolder")+user._id.toString()).to.be.a.directory().with.files([imageFilename, thumbnailFilename]);
         });
 
+        it('it should GET a 400 http code and a credential error as a result because the user readonly does not have credential', async () => {
+            // Arrange
+
+            // Act
+            let res = await chai.request(app).post('/api/images/' + engine._uiId.toString())
+            .field('name', 'my first image added')
+            .field('_uiId', "image_added_01")
+            .field('parentUiId', engine._uiId)
+            .attach('imageData', fs.readFileSync('tests/toUpload/image4.jpeg'), engine._uiId + ".jpeg")
+            .attach('thumbnail', fs.readFileSync('tests/toUpload/thumbnail4.jpeg'), "thumbnail_" + engine._uiId + ".jpeg")
+            .set("Authorization", roUserJWT);
+
+            // Assert
+            res.should.have.status(400);
+            res.body.errors.should.be.eql('credentialError');
+        });
+
         it('it should get a 500 http code error as a result because the user storage is full', async () => {
             // Arrange
             let res = await chai.request(app).post('/api/images/' + engine._uiId.toString())
@@ -450,6 +579,27 @@ describe('Images', () => {
             res.body.image.name.should.be.eql("image1 modified");
         });
 
+        it('it should get a 400 http code and a credential error as a result because the read only user does not have enough credential to edit the image', async () => {
+            // Arrange
+            fs.copyFileSync('tests/toUpload/image1.jpeg', config.get("ImageFolder") + 'image1.jpeg');
+            fs.copyFileSync('tests/toUpload/thumbnail1.jpeg', config.get("ImageFolder") + 'thumbnail1.jpeg');
+
+            const image1Path = config.get("ImageFolder") + "image1.jpeg";
+            const thumbnail1Path = config.get("ImageFolder") + "thumbnail1.jpeg";
+            let image1 = new Images({ _uiId: "image_01", description: "image 1 description", name: "image1", parentUiId: "engine_01" , path:image1Path, thumbnailPath:thumbnail1Path, title:"image1"});
+            image1 = (await image1.save());
+            const jsonImage1 = await image1.toJSON()
+
+            let jsonImage = {name:"image1 modified"};
+            
+            // Act
+            let res = await chai.request(app).post('/api/images/'+ engine._uiId + '/' + jsonImage1._uiId).send({image: jsonImage}).set("Authorization", roUserJWT);
+
+            // Assert
+            res.should.have.status(400);
+            res.body.errors.should.be.eql('credentialError');
+        });
+
         it('it should get a 200 http code as a result because the image description changed successfully', async () => {
             // Arrange
             fs.copyFileSync('tests/toUpload/image1.jpeg', config.get("ImageFolder") + 'image1.jpeg');
@@ -594,6 +744,24 @@ describe('Images', () => {
             res.body.image._uiId.should.be.eql(image1._uiId.toString());
 
             expect(config.get("ImageFolder")).to.be.a.directory().and.empty;
+        });
+
+        it('it should get a 400 http code and a credential error because the read only user does not have credential', async () => {
+            // Arrange
+            fs.copyFileSync('tests/toUpload/image1.jpeg', config.get("ImageFolder") + 'image1.jpeg');
+            fs.copyFileSync('tests/toUpload/thumbnail1.jpeg', config.get("ImageFolder") + 'thumbnail1.jpeg');
+
+            const image1Path = config.get("ImageFolder") + "image1.jpeg";
+            const thumbnail1Path = config.get("ImageFolder") + "thumbnail1.jpeg";
+            let image1 = new Images({ _uiId: "image_01", description: "image 1 description", name: "image1", parentUiId: engine._uiId , path:image1Path, thumbnailPath:thumbnail1Path, title:"image1"});
+            image1 = (await image1.save());
+
+            // Act
+            let res = await chai.request(app).delete('/api/images/'+ engine._uiId + '/' + image1._uiId).set("Authorization", roUserJWT);
+
+            // Assert
+            res.should.have.status(400);
+            res.body.errors.should.be.eql('credentialError');
         });
 
         it('it should get a 400 http code as a result because the image does not exist', async () => {
