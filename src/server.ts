776c185ec2521009a99c6ee5392df486c9b45c4b
paul.torruella <paul.torruella@suez.com>
2021-01-31 00:47:53 +0100

    Schedule the user cleanup using the taskmanager in the server startup

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-
 import App from "./app";
 import AssetsController from "./controllers/assets.controller";
 import EntriesController from "./controllers/entries.controller";
@@ -11,6 +10,14 @@ import UsersController from "./controllers/users.controller";
 import logger from "./utils/logger";
 import getDbVersion from "./utils/mongoDb";
 
+import TaskManager from "./tasks/TaskManager";
+
+import {
+  deleteOrphanGuestUser,
+  deleteUserWhoDidNotUseTheWebApp,
+  sendEmailToUsersWhoAreGoingToBeDeleted,
+} from "./tasks/UserCleanerHelper";
+
 const server = new App(
   [
     new UsersController(),
@@ -22,16 +29,29 @@ const server = new App(
     new AssetsController(),
     new ServerController(),
 ]);
-
 export default server; // for testing
 
+const taskManager: TaskManager = new TaskManager([
+  { cronTabConfig: "0 1 * * *", action: () => sendEmailToUsersWhoAreGoingToBeDeleted(30)},
+  { cronTabConfig: "1 1 * * *", action: () => sendEmailToUsersWhoAreGoingToBeDeleted(15)},
+  { cronTabConfig: "2 1 * * *", action: () => sendEmailToUsersWhoAreGoingToBeDeleted(7)},
+  { cronTabConfig: "3 1 * * *", action: () => sendEmailToUsersWhoAreGoingToBeDeleted(1)},
+  { cronTabConfig: "4 1 * * *", action: deleteUserWhoDidNotUseTheWebApp},
+  { cronTabConfig: "5 1 * * *", action: deleteOrphanGuestUser},
+]);
+
 const tryStartServer = async () => {
+
   try {
     const dbVersion = await getDbVersion();
     logger.info(`The Database version is ${dbVersion}`);
+
+    taskManager.start();
+
     server.listen();
   } catch (error) {
     logger.error(error);
+    taskManager.stop();
   }
 };
 
