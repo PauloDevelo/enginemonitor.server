77a65f8a14a39e863e15469308f5ead9037cc86c
paul.torruella <paul.torruella@suez.com>
2021-02-04 14:00:42 +0100

    Still improving the type experience

--- src/utils/expressHelpers.ts
+++ src/utils/expressHelpers.ts
@@ -1,8 +1,8 @@
-import express from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 // eslint-disable-next-line no-unused-vars
-const wrapAsync = (fn: (_req: express.Request, _res: express.Response, _next: any) => Promise<any>) => {
-  const asyncFuncWrapperHandler = (req: express.Request, res: express.Response, next) => {
+const wrapAsync = (fn: (_req: Request, _res: Response, _next: NextFunction) => Promise<any>) => {
+  const asyncFuncWrapperHandler = (req: Request, res: Response, next: NextFunction): any => {
     // Make sure to `.catch()` any errors and pass them along to the `next()`
     // middleware in the chain, in this case the error handler.
     fn(req, res, next).catch(next);
